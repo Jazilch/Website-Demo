14837e868bd6087e3c4e278090cff1420fdbf6b4
Jazilch <zilchjames5@gmail.com>
2018-02-18 17:18:31 -0500

    Update FinalWebsitegit.js

--- FinalWebsitegit.js
+++ FinalWebsitegit.js
@@ -14,7 +14,7 @@ window.onload = headerFade;
 
 const slideMenu = document.querySelector('.slideInMenu');
 
-function slideInMenu(e) {
+function slideInMenu() {
 	if (window.scrollY > 100) {
 		slideMenu.classList.add('slideMenu-active');
 	} else {
@@ -26,37 +26,62 @@ window.addEventListener('scroll', slideInMenu);
 
 // Slide In Title Texts 
 
-const slideText = document.querySelector('#fadeInText');
+// const slideText = document.querySelector('#fadeInText');
 
-function textSlide(e) {
-	if (window.scrollY > 293){
-		slideText.classList.add('active');
-	} else {
-		slideText.classList.remove('active');
-	}
-}
-
-window.addEventListener('scroll', textSlide);
-
-// Slide In Banner Texts 
-
-// const slideTexts = document.querySelectorAll('#fadeInText');
-
-// function textSlide(e) {
-// 	slideTexts.forEach(slideText => {
-// 	const slideInAt = (window.scrollY + window.innerHeight) - slideText.height / 2;
-// 	const textBottom =  slideText.offsetTop + slideText.height;
-// 	const isHalfShown = slideInAt > slideText.offsetTop;
-// 	const isNotScrolledPast = window.scrollY < textBottom;
-// 	if (isHalfShown && isNotScrolledPast) {
+// function textSlide() {
+// 	if (window.scrollY > 293){
 // 		slideText.classList.add('active');
 // 	} else {
 // 		slideText.classList.remove('active');
 // 	}
-// 	});
 // }
 
 // window.addEventListener('scroll', textSlide);
+function debounce(func, wait = 20, immediate = true) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    };
+
+  var fadeInText = document.querySelectorAll('#fadeInText');
+
+    function checkSlide(e) {
+      fadeInText.forEach(fadeInText => {
+        // half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - fadeInText.offsetHeight / 2;
+        console.log(slideInAt)
+        // bottom of the image
+        const bannerBottom = fadeInText.offsetTop + fadeInText.offsetHeight;
+        const isHalfShown = slideInAt > fadeInText.offsetTop;
+        const isNotScrolledPast = window.scrollY < bannerBottom;
+        if (isHalfShown && isNotScrolledPast) {
+          fadeInText.classList.add('active');
+        } else {
+          fadeInText.classList.remove('active');
+        }
+      });
+    }
+    window.addEventListener('scroll', debounce(checkSlide));
+
+// Mobile Menu JS
+const mobileIcon = document.querySelector('.mobileIcon');
+
+function showMenu(e) {
+	const mobileMenu = document.querySelector('.mobileMenu');
+	mobileMenu.classList.toggle('show-mobile-menu');
+}
+
+mobileIcon.addEventListener('click', showMenu);
+
 
 // Modal Window Open
 
